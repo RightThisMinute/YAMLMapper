c5c74fa4c05916e9d997f62c24d1734dd99ea3fd
Donovan Mueller <zotobi@gmail.com>
2016-11-10 16:42:03 -0700

    Add .git to Mapper dependency package.
    This was causing problem when this package is included as a dependency
    along side Mapper. If the parent project has .git on the Mapper URL
    SPM gets confused and throws an error like this:
    
    > error: rename error: Directory not empty (66): /Users/me/Example/Packages/Mapper -> /Users/me/Example/Packages/Mapper-0.14.0

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
 	name: "YAMLMapper",
 	dependencies: [
-		.Package(url: "https://github.com/Zewo/Mapper",
+		.Package(url: "https://github.com/Zewo/Mapper.git",
 		         majorVersion: 0, minor: 14),
 		.Package(url: "https://github.com/behrang/YamlSwift.git",
 		         majorVersion: 3, minor: 0),
